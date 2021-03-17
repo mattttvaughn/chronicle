f4c3c47157ea2f4d95625c549a2758e0f402bf12
Matt Vaughn <mattttvaughn@gmail.com>
2021-03-17 12:08:06 -0700

    Rename FlowableRadioGroupExt.kt to ChipGroupExt.kt

--- app/src/main/java/io/github/mattpvaughn/chronicle/views/ChipGroupExt.kt
+++ app/src/main/java/io/github/mattpvaughn/chronicle/views/ChipGroupExt.kt
@@ -1,11 +1,12 @@
 package io.github.mattpvaughn.chronicle.views
 
-import android.widget.RadioButton
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import timber.log.Timber
 
-fun FlowableRadioGroup.checkRadioButtonWithTag(o: Any) {
+fun ChipGroup.checkRadioButtonWithTag(o: Any) {
     for (x in 0 until childCount) {
-        val tempChild = getChildAt(x) as RadioButton
+        val tempChild = getChildAt(x) as Chip
         Timber.i("Child tag is ${tempChild.tag}")
         if (tempChild.tag == o) {
             check(tempChild.id)
