0acf665797fbab5143c57df5e629414e0237a508
Matt Vaughn <mattttvaughn@gmail.com>
2022-02-15 21:12:42 -0500

    Enable Auto by default

--- app/src/main/java/io/github/mattpvaughn/chronicle/application/Constants.kt
+++ app/src/main/java/io/github/mattpvaughn/chronicle/application/Constants.kt
@@ -2,7 +2,7 @@ package io.github.mattpvaughn.chronicle.application
 
 import io.github.mattpvaughn.chronicle.BuildConfig
 
-const val FEATURE_FLAG_IS_AUTO_ENABLED = false
+const val FEATURE_FLAG_IS_AUTO_ENABLED = true
 const val FEATURE_FLAG_IS_CAST_ENABLED = false
 const val MILLIS_PER_SECOND = 1000L
 const val SECONDS_PER_MINUTE = 60L
