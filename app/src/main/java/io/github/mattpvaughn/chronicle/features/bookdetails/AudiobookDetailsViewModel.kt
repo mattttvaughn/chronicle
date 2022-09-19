mattttvaughn/chronicle

commit 659e8ec390020e70d6526159634fcb7811a6fd37

Progress percentage (#86)
* Added percentage complete to AudiobookDetails

* Added progress percent to currently playing fragment_currently_playing.xml

Credit: @AnDr3w7911

app/src/main/java/io/github/mattpvaughn/chronicle/features/bookdetails/AudiobookDetailsViewModel.kt
@@ -38,6 +38,7 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.combine
 import timber.log.Timber
 import javax.inject.Inject
+import kotlin.math.roundToInt
 
 @ExperimentalCoroutinesApi
 class AudiobookDetailsViewModel(
@@ -167,12 +168,16 @@ class AudiobookDetailsViewModel(
         }
 
     val progressString = Transformations.map(tracks) { tracks: List<MediaItemTrack> ->
-        if (tracks.isNullOrEmpty()) {
+        if (tracks.isEmpty()) {
             return@map "0:00/0:00"
         }
         val progressStr = DateUtils.formatElapsedTime(StringBuilder(), tracks.getProgress() / 1000L)
         val durationStr = DateUtils.formatElapsedTime(StringBuilder(), tracks.getDuration() / 1000L)
         return@map "$progressStr/$durationStr"
+    }
+
+    val progressPercentageString = Transformations.map(tracks) { tracks: List<MediaItemTrack> ->
+        return@map "${tracks.getProgressPercentage()}%"
     }
 
     private var _isLoadingTracks = MutableLiveData(false)

app/src/main/java/io/github/mattpvaughn/chronicle/data/model/MediaItemTrack.kt
@@ -8,9 +8,11 @@ import io.github.mattpvaughn.chronicle.data.local.ITrackRepository.Companion.TRA
 import io.github.mattpvaughn.chronicle.data.model.MediaItemTrack.Companion.EMPTY_TRACK
 import io.github.mattpvaughn.chronicle.data.sources.plex.PlexConfig
 import io.github.mattpvaughn.chronicle.data.sources.plex.model.PlexDirectory
+import io.github.mattpvaughn.chronicle.data.sources.plex.model.getDuration
 import io.github.mattpvaughn.chronicle.features.player.*
 import timber.log.Timber
 import java.io.File
+import kotlin.math.roundToInt
 
 /**
  * A model for an audio track (i.e. a song)
@@ -183,7 +185,7 @@ fun List<MediaItemTrack>?.getTrackContainingOffset(offset: Long): MediaItemTrack
 }
 
 /**
- * Return the progress of the current track plus the duration of all previous tracks
+ * @return the progress of the current track plus the duration of all previous tracks
  */
 fun List<MediaItemTrack>.getProgress(): Long {
     if (isEmpty()) {
@@ -192,6 +194,16 @@ fun List<MediaItemTrack>.getProgress(): Long {
     val currentTrackProgress = getActiveTrack().progress
     val previousTracksDuration = getTrackStartTime(getActiveTrack())
     return currentTrackProgress + previousTracksDuration
+}
+
+/**
+ * @return progress as percent
+ */
+fun List<MediaItemTrack>.getProgressPercentage(): Int {
+    if (isEmpty() || getDuration() == 0L) {
+        return 0
+    }
+    return ((getProgress() / getDuration().toDouble()) * 100).roundToInt()
 }
 
 /**

app/src/main/java/io/github/mattpvaughn/chronicle/data/sources/plex/model/TrackPlexModel.kt
@@ -19,8 +19,9 @@ fun List<PlexDirectory>?.asMediaItemTracks(): List<MediaItemTrack> {
     } ?: emptyList()
 }
 
+/**
+ * @return the total duration of all the tracks
+ */
 fun List<MediaItemTrack>.getDuration(): Long {
-    var acc = 0L
-    this.forEach { track -> acc += track.duration }
-    return acc
+    return this.asSequence().map(MediaItemTrack::duration).sum()
 }

app/src/main/java/io/github/mattpvaughn/chronicle/features/currentlyplaying/CurrentlyPlayingViewModel.kt
@@ -215,12 +215,17 @@ class CurrentlyPlayingViewModel(
         return@map DateUtils.formatElapsedTime(StringBuilder(), track.duration / 1000)
     }
 
-    val bookProgressString = Transformations.map(tracks) {
-        return@map DateUtils.formatElapsedTime(StringBuilder(), it.getProgress() / 1000)
+    val progressString = Transformations.map(tracks) { tracks: List<MediaItemTrack> ->
+        if (tracks.isEmpty()) {
+            return@map "0:00/0:00"
+        }
+        val progressStr = DateUtils.formatElapsedTime(StringBuilder(), tracks.getProgress() / 1000L)
+        val durationStr = DateUtils.formatElapsedTime(StringBuilder(), tracks.getDuration() / 1000L)
+        return@map "$progressStr/$durationStr"
     }
 
-    val bookDurationString = Transformations.map(audiobook) {
-        return@map DateUtils.formatElapsedTime(StringBuilder(), it?.duration?.div(1000) ?: 0)
+    val progressPercentageString = Transformations.map(tracks) { tracks: List<MediaItemTrack> ->
+        return@map "${tracks.getProgressPercentage()}%"
     }
 
     private val cachedChapter = DoubleLiveData(

app/src/main/res/layout/fragment_audiobook_details.xml
@@ -78,10 +78,22 @@
                         style="@style/TextAppearance.Body2"
                         android:layout_width="wrap_content"
                         android:layout_height="wrap_content"
+                        android:layout_marginEnd="8dp"
                         android:text="@{viewModel.progressString}"
+                        app:layout_constraintEnd_toStartOf="@+id/progressPercentage"
                         app:layout_constraintRight_toRightOf="@id/details_artwork"
                         app:layout_constraintTop_toBottomOf="@id/details_artwork"
                         tools:text="1:21/3:52" />
+
+                    <TextView
+                        android:id="@+id/progressPercentage"
+                        style="@style/TextAppearance.Body2"
+                        android:layout_width="wrap_content"
+                        android:layout_height="wrap_content"
+                        android:text="@{viewModel.progressPercentageString}"
+                        app:layout_constraintRight_toRightOf="@id/details_artwork"
+                        app:layout_constraintTop_toBottomOf="@id/details_artwork"
+                        tools:text="35%" />
 
                     <FrameLayout
                         android:id="@+id/download_indicator_container"

app/src/main/res/layout/fragment_currently_playing.xml
@@ -178,11 +178,23 @@
                         style="@style/TextAppearance.Body2"
                         android:layout_width="wrap_content"
                         android:layout_height="wrap_content"
-                        android:text="@{viewModel.bookProgressString + '/' + viewModel.bookDurationString}"
+                        android:layout_marginRight="8dp"
+                        android:text="@{viewModel.progressString}"
                         android:visibility="@integer/currently_playing_artwork_visibility"
+                        app:layout_constraintEnd_toStartOf="@+id/progressPercentage"
                         app:layout_constraintRight_toRightOf="@id/details_artwork"
                         app:layout_constraintTop_toBottomOf="@id/details_artwork"
                         tools:text="1:21/3:52" />
+
+                    <TextView
+                        android:id="@+id/progressPercentage"
+                        style="@style/TextAppearance.Body2"
+                        android:layout_width="wrap_content"
+                        android:layout_height="wrap_content"
+                        android:text="@{viewModel.progressPercentageString}"
+                        app:layout_constraintRight_toRightOf="@id/details_artwork"
+                        app:layout_constraintTop_toBottomOf="@id/details_artwork"
+                        tools:text="35%" />
 
                     <TextView
                         android:id="@+id/chapter_progress"

commit 4e0862983e9c56c9535c4e6d28ac593d1a36e683

Fix Kotlin Lint issue to pass precommit hook
Clean up unused import introduced in commit 659e8ec390020e70d6526159634fcb7811a6fd37

https://github.com/mattttvaughn/chronicle/commit/659e8ec390020e70d6526159634fcb7811a6fd37#diff-843bb7b0e947d711cf8a353b00da93b84c923587922b465ea4401d1b7ec39ff9

app/src/main/java/io/github/mattpvaughn/chronicle/features/bookdetails/AudiobookDetailsViewModel.kt
@@ -38,7 +38,6 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.combine
 import timber.log.Timber
 import javax.inject.Inject
-import kotlin.math.roundToInt
 
 @ExperimentalCoroutinesApi
 class AudiobookDetailsViewModel(