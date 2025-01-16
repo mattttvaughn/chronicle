c2f6c1c9d0b5b38a712a2a02898bc7b055c42a1b
Zachary Coumont <me@zacharycoumont.com>
2025-01-16 05:01:13 -0800

    Upgrade dependencies to fix issue when minifying builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
